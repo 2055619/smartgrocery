0c461675bb9811be273e21f33766f30a748d3c86
2055619 <2055619@claurendeau.qc.ca>
2023-05-26 21:38:04 -0400

    Nouvelle app à cause de clée manquante

--- liste_epicerie/android/app/src/main/kotlin/ca/cal/tp4/lpf/liste_epicerie/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package ca.cal.tp4.lpf.liste_epicerie
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ smartcart/android/app/src/main/kotlin/thequixoticone/apps/smartcart/smartcart/MainActivity.kt
@@ -0,0 +1,6 @@
+package thequixoticone.apps.smartcart.smartcart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
